2a254358606756c462da93cb8d731a3710f6d4b5
Seniru Pasan Indira <34127015+Seniru@users.noreply.github.com>
2019-04-07 12:14:17 +0530

    Simplified and improved the code
    Improved the input testing and simplified the DOM manipulations

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
+let todoList = document.getElementById('todo-list');
+
 function addItem() {
     // get the todo-item-text value
     let newItemText = document.getElementById('todo-item-text').value;
 
-    // check if the newItemText is empty
-    if (newItemText === '') {
+    // check if the newItemText is empty and not a string just with spaces
+    if (newItemText === '' && !(/^\s+$/.test(newItemText))) {
         alert('You cannot add an empty todo item you silly goose!');
-        return;
+    } else {
+       //inserting a single string contains all the elements to the todoList
+        todoList.innerHTML += `
+            <li class="todo-item"><button onclick="deleteItem(this)" class="button-delete">X</button> ${newItemText} <span class="item-date">${new Date().toDateString()}</span></li>
+        `;
+    
     }
 
-    let todoList = document.getElementById('todo-list');
-
-    let newListItem = document.createElement('li');
-    newListItem.classList.add('todo-item');
-
-    let deleteButton = document.createElement('button');
-    deleteButton.classList.add('button-delete');
-    deleteButton.innerHTML = 'X';
-    deleteButton.setAttribute('onclick', 'deleteItem(this)');
-
-    newListItem.appendChild(deleteButton);
-
-    let itemPElement = document.createElement('span');
-    itemPElement.innerHTML = newItemText;
-    newListItem.appendChild(itemPElement);
-
-    let dateElement = document.createElement('span');
-    dateElement.classList.add('item-date');
-    dateElement.innerHTML = (new Date()).toDateString();
-    newListItem.appendChild(dateElement);
-
-    todoList.appendChild(newListItem);
+    
 }
 
 function deleteItem(item) {
